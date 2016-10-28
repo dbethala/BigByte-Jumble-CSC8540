0c28ff8791dbd9d27e16622d52be2c5331888306
Justin Wyrobek <jwyrob01@villanova.edu>
2016-10-27 21:09:43 -0400

    Textfield grid panes

--- New/src/application/Controller.java
+++ New/src/application/Controller.java
@@ -1,29 +1,39 @@
 package application;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.*;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 public class Controller {
+	static final String[] wordsEasy = new String[]{"HAT", "MAP", "HAM"};
+	static final String[] wordsEasy_NotArranged = new String[wordsEasy.length];
 	
-
-	static final String[] wordsstack_Advanced = new String[]{"READ","MOUSE","APPLE","NICE","SPIDER"};
-	static final String[] wordsstack_Advanced_NotArranged = new String[wordsstack_Advanced.length];
-	// first thing to do is to creat connection to the main class
+	static final String[] wordsMedium = new String[]{"WHALE", "GRASS", "LAND", "WALL"};
+	static final String[] wordsMedium_NotArranged = new String[wordsMedium.length];
+    
+        static final String[] wordsHard = new String[]{"READ","MOUSE","APPLE","NICE","SPIDER"};
+	static final String[] wordsHard_NotArranged = new String[wordsHard.length];
+	
+	// First thing to do is to create connection to the main class
 	private Main main;
-	// views
+	
+	// Views
 	@FXML private Label label; // same as the id i gave it in the scenebuilder
 	@FXML private Label label1;
 	@FXML private Label label2;
 	@FXML private Label label3;
 	@FXML private Label label4;
+	@FXML private Label label5;
 
-	@FXML private TextField field;
+	@FXML private TextField tf1, tf2, tf3, tf4, tf5, tf6;
+	@FXML private TextField tf7, tf8, tf9, tf10, tf11, tf12;
+	@FXML private TextField tf13, tf14, tf15, tf16, tf17, tf18;
+	@FXML private TextField tf19, tf20, tf21, tf22, tf23, tf24;
+	@FXML private TextField tf25, tf26, tf27, tf28, tf29, tf30;
+	
 	public void setMain(Main main){
 	this.main=main; // now the main class is connected with the controller now i can call any method form the main	
 	}
-	
-	
 	
 	public void handleButton(){
 		
@@ -31,23 +41,20 @@ public class Controller {
 		label2.setVisible(true);
 		label3.setVisible(true);
 		label4.setVisible(true);
-		Words word= new Words();
-		for(int i=0; i<wordsstack_Advanced.length;i++)
-		{
 		
-			word.shuffleword(wordsstack_Advanced[i]);
+		Words word = new Words();
+		
+		for(int i = 0; i < wordsHard.length; i++)
+		{
+			word.shuffleword(wordsHard[i]);
 			
-			wordsstack_Advanced_NotArranged[i] = word.shuffleword(wordsstack_Advanced[i]); //saving each notarranged word into the not arranged stack
-			System.out.println(word.shuffleword(wordsstack_Advanced[i]));
+			wordsHard_NotArranged[i] = word.shuffleword(wordsHard[i]); //saving each notarranged word into the not arranged stack
+			System.out.println(word.shuffleword(wordsHard[i]));
 		}
 		
-		String text= field.getText();
-		label1.setText(wordsstack_Advanced_NotArranged[0]);
-		label2.setText(wordsstack_Advanced_NotArranged[1]);
-		label3.setText(wordsstack_Advanced_NotArranged[2]);
-		label4.setText(wordsstack_Advanced_NotArranged[3]);
-
-		field.clear();
-		
+		label1.setText(wordsHard_NotArranged[0]);
+		label2.setText(wordsHard_NotArranged[1]);
+		label3.setText(wordsHard_NotArranged[2]);
+		label4.setText(wordsHard_NotArranged[3]);
 	}
 }
--- New/src/application/Main.java
+++ New/src/application/Main.java
@@ -13,7 +13,9 @@ import javafx.scene.layout.BorderPane;
 public class Main extends Application {
 	
 	
+	//static final String[] wordsstack_Advanced = new String[]{"ice","car","pen","game","ring"};
 
+	
 	
 	private Stage primaryStage;
 	@Override
@@ -27,18 +29,12 @@ public class Main extends Application {
 	try {
 		//primstage is the window itself but the scene is the content 
 		FXMLLoader loader = new FXMLLoader(Main.class.getResource("sample.fxml")); // if it did not work put/
-		//FXMLLoader loader2 = new FXMLLoader(Main.class.getResource("sample2.fxml")); // if it did not work put/
+		
 		AnchorPane pane = loader.load();
-		//AnchorPane pane = loader2.load();
 		Controller controllerobj = loader.getController();
-		//Controller controllerobj = loader2.getController();
 		controllerobj.setMain(this); // now whenever te main start it also call th method setMain from the controller class
 		Scene scene = new Scene(pane); // scene take the pane as argument bcause pane load all the elements like buttons.etc
-		
-		
-		
 		primaryStage.setScene(scene);
-		primaryStage.setTitle("Jumble Game");
 		primaryStage.show();
 		
 	} catch (IOException e) {
@@ -49,7 +45,13 @@ public class Main extends Application {
 	}
 	
 	public static void main(String[] args) {
-
+//		Words word= new Words();
+//		for(int i=0; i<wordsstack_Advanced.length;i++)
+//		{
+//		
+//			word.shuffleword(wordsstack_Advanced[i]);
+//			System.out.println(word.shuffleword(wordsstack_Advanced[i]));
+//		}
 		launch(args);
 	}
 }
