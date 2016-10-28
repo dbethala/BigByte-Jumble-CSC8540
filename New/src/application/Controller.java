067ac029eaf94e1f1b67482240d436ce2a315381
David Bethala <david.bethala@icloud.com>
2016-10-27 23:44:25 -0400

    Merge remote-tracking branch 'origin/Justin' into David
    # Conflicts:
    #	Jumble/bin/.gitignore
    #	New/bin/application/Controller.class
    #	New/bin/application/Main.class
    #	New/bin/application/sample.fxml
    #	New/src/application/Controller.java
    #	New/src/application/Main.java
    #	New/src/application/sample.fxml

--- New/src/application/Controller.java
+++ New/src/application/Controller.java
@@ -1,7 +1,6 @@
 package application;
 
 import javafx.animation.AnimationTimer;
-import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javax.swing.Timer;
 
@@ -9,32 +8,46 @@ import javafx.scene.control.*;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 
-import java.awt.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Date;
 import java.util.TimerTask;
 import java.util.*;
-public class Controller {
-	
 
-	static final String[] wordsstack_Advanced = new String[]{"READ","MOUSE","APPLE","NICE","SPIDER"};
-	static final String[] wordsstack_Advanced_NotArranged = new String[wordsstack_Advanced.length];
-	// first thing to do is to creat connection to the main class
+public class Controller {
+	static final String[] wordsEasy = new String[]{"HAT", "MAP", "HAM"};
+	static final String[] wordsEasy_NotArranged = new String[wordsEasy.length];
+	
+	static final String[] wordsMedium = new String[]{"WHALE", "GRASS", "LAND", "WALL"};
+	static final String[] wordsMedium_NotArranged = new String[wordsMedium.length];
+    
+        static final String[] wordsHard = new String[]{"READ","MOUSE","APPLE","NICE","SPIDER"};
+	static final String[] wordsHard_NotArranged = new String[wordsHard.length];
+	
+	// First thing to do is to create connection to the main class
 	private Main main;
-	// views
+	
+	// Views
 	@FXML private Label label; // same as the id i gave it in the scenebuilder
 	@FXML private Label label1;
 	@FXML private Label label2;
 	@FXML private Label label3;
 	@FXML private Label label4;
+	@FXML private Label label5;
 
-	@FXML private TextField field;
+	@FXML private TextField tf1, tf2, tf3, tf4, tf5, tf6;
+	@FXML private TextField tf7, tf8, tf9, tf10, tf11, tf12;
+	@FXML private TextField tf13, tf14, tf15, tf16, tf17, tf18;
+	@FXML private TextField tf19, tf20, tf21, tf22, tf23, tf24;
+	@FXML private TextField tf25, tf26, tf27, tf28, tf29, tf30;
+	
 	@FXML private Text timerField;
+	
 	public void setMain(Main main){
 	this.main=main; // now the main class is connected with the controller now i can call any method form the main	
 	}
-	
-	
 	
 	public void handleButton(){
 		
@@ -42,26 +55,24 @@ public class Controller {
 		label2.setVisible(true);
 		label3.setVisible(true);
 		label4.setVisible(true);
-		Words word= new Words();
-		for(int i=0; i<wordsstack_Advanced.length;i++)
-		{
 		
-			word.shuffleword(wordsstack_Advanced[i]);
+		Words word = new Words();
+		
+		for(int i = 0; i < wordsHard.length; i++)
+		{
+			word.shuffleword(wordsHard[i]);
 			
-			wordsstack_Advanced_NotArranged[i] = word.shuffleword(wordsstack_Advanced[i]); //saving each notarranged word into the not arranged stack
-			System.out.println(word.shuffleword(wordsstack_Advanced[i]));
+			wordsHard_NotArranged[i] = word.shuffleword(wordsHard[i]); //saving each notarranged word into the not arranged stack
+			System.out.println(word.shuffleword(wordsHard[i]));
 		}
 		
-		String text= field.getText();
-		label1.setText(wordsstack_Advanced_NotArranged[0]);
-		label2.setText(wordsstack_Advanced_NotArranged[1]);
-		label3.setText(wordsstack_Advanced_NotArranged[2]);
-		label4.setText(wordsstack_Advanced_NotArranged[3]);
-		setTimer();
-		field.clear();
+		label1.setText(wordsHard_NotArranged[0]);
+		label2.setText(wordsHard_NotArranged[1]);
+		label3.setText(wordsHard_NotArranged[2]);
+		label4.setText(wordsHard_NotArranged[3]);
 		
+		setTimer();
 	}
-	
 	//Method to set timer. Implements AnimationTimer to spawn new thread to update UI
 	private void setTimer() {
 		new AnimationTimer(){
@@ -83,7 +94,7 @@ public class Controller {
 				// TODO Auto-generated method stub
 				long now = System.currentTimeMillis();	//Gets current time after thread has started
 				Long difference = (now-startTime)/1000;	//1000 milliseconds in one second
-				timerField.setText(difference.toString() + "s");	//Appends the "s" and prints time
+				timerField.setText(difference.toString() + "s");	//Appends the "s" and prints time in seconds
 			}
 		}.start();
 	}
